9e52235035a6d503baa0a7746f8fbd177da0afde
sajede2003 <sajededarzinia@gmail.com>
2022-03-06 23:26:41 +0330

    created estate model

---
+++ app/Models/estate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class estate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'img',
+        'category_id',
+        'bargain_id',
+        'properties',
+        'status'
+    ];
+}
