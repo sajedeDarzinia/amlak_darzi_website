ad7b7ae6aa2618b8a3094b26687558c7cb320647
sajede2003 <sajededarzinia@gmail.com>
2022-04-23 12:27:25 +0430

    created auth view page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+@section('sign_up')
+    <title>SignUp</title>
+    <div class="form-container sign-up-container">
+        <form action="/register" method="post">
+            @csrf
+            <h1>ثبت نام کنید</h1>
+            <div class="social-container">
+                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
             </div>
-        </div>
+            <span>یا با ایمیل خود ثبت نام کنبد</span>
+            <input type="text" placeholder="نام کاربری"/>
+            <input type="email" placeholder="ایمیل"/>
+            <input type="password" placeholder="گذرواژه"/>
+            <button>ثبت نام</button>
+        </form>
     </div>
-</div>
+
 @endsection
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/auth_style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+
+<div class="container" id="container">
+    {{--    login : sign_in--}}
+    @yield('sign_in')
+    {{--    register : sign_up --}}
+    @yield('sign_up')
+    <script type="text/javascript">
+        const cont = document.getElementById('container');
+        if (document.title === "SignUp") {
+            setTimeout(function () {
+                cont.classList.add('right-panel-active');
+            }, 0.5);
+        }
+    </script>
+    <div class="overlay-container">
+        <div class="overlay">
+            {{--            sign_in_button--}}
+            <div class="overlay-panel overlay-left">
+                <h1>حساب کاربری دارید؟</h1>
+                <p>اگر قبلا در ثبت نام کرده اید می توانید با وارد کردن اطلاعات حساب خود وارد شوید.</p>
+                <a class="ghost" id="signIn" href="/login">ورود</a>
+            </div>
+            {{--            sign_up_button--}}
+            <div class="overlay-panel overlay-right">
+                <h1>کاربر جدید هستید؟</h1>
+                <p>ابتدا باید ثبت نام کنید.</p>
+                <a class="ghost" id="signUp" href="/register">ثبت نام</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!------------JS!------------>
+<script src="{{ asset('js/auth.js') }}"></script>
+
+</body>
+
+</html>
