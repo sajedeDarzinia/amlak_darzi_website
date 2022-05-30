6ef1be90ccddb4f67486852f7968582a1af17132
sajede2003 <sajededarzinia@gmail.com>
2022-05-30 23:56:17 +0430

    edit js file

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,6 +1,4 @@
-
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-
 
 signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
