4f0ffb0074840e7f25cdd245ebafa67d5f3b8657
sajede2003 <sajededarzinia@gmail.com>
2022-05-30 23:54:39 +0430

    create google auth routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('auth/google' ,[\App\Http\Controllers\Auth\GoogleAuthController::class,'redirect'] )->name('auth.google');
+Route::get('auth/google/callback' ,[\App\Http\Controllers\Auth\GoogleAuthController::class,'callback'] );
